60cc5d5f6b158cc068389ebeebac46ffc3b37a33
mygoare <mygoare@gmail.com>
2013-10-17 13:32:14 +0800

    if links.empty? break

--- capture.rb
+++ capture.rb
@@ -4,13 +4,18 @@ require "open-uri"
 require "uri"
 
 douban_url = URI.escape("http://www.douban.com/group/search?group=146409&cat=1013&q=静安")
-t = Time.new(2013,9,12)
+t = Time.now()
 
 open(douban_url) { |f|
   content = f.read()
   links = content.scan(/<tr class=\"pl\">[\s\S]*?<\/tr>/).flatten
 
   links = links.select { |link| link =~ (Regexp.new t.strftime("%Y-%m-%d")) }
+
+  if links.empty?
+    puts "今天没有相关房源更新！"
+    break;
+  end
 
   links.each do |link|
     href = /<a class="" href="(.*)" title="(.*)">/.match(link)[1]
